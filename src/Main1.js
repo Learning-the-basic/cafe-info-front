90138c768b6a2441f0d44db7c2d9ad514e5cc201
MODIFYC <chsj5742@naver.com>
2022-05-21 11:48:49 +0900

    commit4

--- src/Main1.js
+++ src/Main1.js
@@ -2,6 +2,7 @@ import './Main1.css';
 
 function Main1() {
   return (
+    // 새로고침 시 랜덤으로 배경이 바뀝니다.
     <div className='wrap'>
       {/* 1. header */}
       <div className="header">
@@ -12,11 +13,11 @@ function Main1() {
         </a>
         <div className="login">
           {/* 1-2. 사업자 로그인 */}
-          {/* 사업자 로그인 페이지로 이동합니다. */}
+          {/* 사업자 로그인 페이지(Businesslogin.js)로 이동합니다. */}
           <span>사업자 로그인</span>
           <b> | </b>
           {/* 1-3. 일반 회원 로그인 */}
-          {/* 일반 회원 페이지로 이동합니다. */}
+          {/* 일반 회원 페이지(MemberLogin.js)로 이동합니다. */}
           <span>일반 회원 로그인</span>
         </div>
       </div>
@@ -26,19 +27,19 @@ function Main1() {
       <div className="search">
         {/* 2-1. search bar */}
         {/* 검색 클릭 시 UI 변경 되고 검색 버튼 추가 필요합니다. */}
-        {/* 검색 버튼 클릭 시 검색 페이지 이동됩니다 */}
+        {/* 검색 버튼 클릭 시 검색 페이지(Search.js) 이동됩니다 */}
         <div className="searchBar">검색</div>
         {/* 2-2. keyword */}
-        {/* #뒤에 키워드(고정?변동?) 추가 후 클릭 시 키워드에 맞는 페이지 이동합니다. */}
+        {/* #뒤에 키워드(고정?변동?) 추가 후 클릭 시 키워드에 맞는 페이지(Search.js) 이동합니다. */}
         <div className="keyword">추천키워드: #{ } #{ } #{ }</div>
       </div>
 
       {/* 3. nav */}
       <div className="nav">
         <div className="nav1"></div>
-        {/* Main2.js으로 이동합니다. */}
+        {/* 두번 째 메인페이지(Main2.js)으로 이동합니다. */}
         <div className="nav2"></div>
-        {/* Main3.js로 이동합니다. */}
+        {/* 세번 째 메인페이지(Main3.js)로 이동합니다. */}
         <div className="nav3"></div>
       </div>
     </div>
